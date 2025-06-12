f73e803f9e7f5d3c084ad381324e1e994d6cbb6e
Talin Sharma <talinsharma.dev@gmail.com>
2025-06-12 02:34:38 +0000

    Moving circle with decent although not realistic window border collision checking and white background with orange ball!

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,41 @@
 const std = @import("std");
+
 const lib = @import("pong_lib");
 const rl = @import("raylib");
 
-pub fn main() !void {
-    const width = 800;
-    const height = 450;
+const BALL_VELOCITY = 500;
 
-    rl.initWindow(width, height, "Pong");
+pub fn main() !void {
+    rl.initWindow(800, 450, "Pong");
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
 
+    var ball_negate: i8 = 1;
+    var x: i32 = 375;
+    var y: i32 = 200;
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(rl.Color.sky_blue);
-        std.debug.print("Current FPS: {}\n", .{rl.getFPS()});
+        const WIDTH = rl.getRenderWidth();
+        const HEIGHT = rl.getRenderHeight();
 
-        const x: i32 = @intFromFloat(rl.getTime() * 100);
-        rl.drawCircle(0 + x, 0 + x, 25, rl.Color.white);
+        const ball_delta: i32 = @intFromFloat(BALL_VELOCITY * rl.getFrameTime());
+        x = x + (ball_negate * ball_delta);
+        y = y + (ball_negate * ball_delta);
+
+        if (y > HEIGHT)
+            ball_negate = -1;
+        if (x > WIDTH)
+            ball_negate = -1;
+        if (y < 0)
+            ball_negate = 1;
+        if (x < 0)
+            ball_negate = 1;
+
+        rl.drawCircle(x, y, 25, rl.Color.orange);
+
+        rl.clearBackground(rl.Color.white);
     }
 }
