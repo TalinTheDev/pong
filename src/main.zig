cada7155bbfebd9467382d51732fc388cf2ff865
Talin Sharma <talinsharma.dev@gmail.com>
2025-06-12 03:04:59 +0000

    Added rectangle (paddle left)

--- src/main.zig
+++ src/main.zig
@@ -18,6 +18,14 @@ pub fn main() !void {
 
         lib.ball.drawBall(HEIGHT, WIDTH);
 
+        const rec: rl.Rectangle = rl.Rectangle{
+            .x = 0,
+            .y = 0,
+            .height = 100,
+            .width = 10,
+        };
+        rl.drawRectangleRec(rec, rl.Color.blue);
+
         rl.clearBackground(rl.Color.white);
     }
 }
